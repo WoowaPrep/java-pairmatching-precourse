810eba8307e02848691841fdaa2d1e2c62fce89d
dongheeHan <ark182818@gmail.com>
2026-01-06 22:17:13 +0900

    feat: 미션 필드 재정의

--- src/main/java/pairmatching/domain/Mission.java
+++ src/main/java/pairmatching/domain/Mission.java
@@ -1,34 +1,45 @@
 package pairmatching.domain;
 
 import java.util.Arrays;
+import java.util.List;
 import pairmatching.exception.ErrorMessage;
 import pairmatching.exception.PairmatchingException;
 
 public enum Mission {
 
-    LEVEL1_MISSION("레벨1"),
-    LEVEL2_MISSION("레벨2"),
-    LEVEL3("레벨3"),
-    LEVEL4("레벨4"),
-    LEVEL5("레벨5"),
+    CAR_RACE(Level.LEVEL1, "자동차경주"),
+    LOTTO(Level.LEVEL1, "로또"),
+    NUMBER_BASEBALL_GAME(Level.LEVEL1, "숫자야구게임"),
+
+    SHOPPING_BASKET(Level.LEVEL2, "장바구니"),
+    PAYMENT(Level.LEVEL2, "결제"),
+    SUBWAY_MAP(Level.LEVEL2, "지하철노선도"),
+
+    PERFORMANCE_IMPROVEMENT(Level.LEVEL4, "성능개선"),
+    DISTRIBUTION(Level.LEVEL4, "배포"),
     ;
 
+    private final Level level;
     private final String name;
 
-    Mission(String name) {
+    public static Mission from(Level level, String name) {
+        return Arrays.stream(values())
+                .filter(mission -> mission.getLevel() == level)
+                .filter(mission -> mission.getName().equals(name))
+                .findFirst()
+                .orElseThrow(() -> PairmatchingException.from(ErrorMessage.INVALID_MISSION));
+    }
+
+    Mission(Level level, String name) {
+        this.level = level;
         this.name = name;
     }
 
-    public static Mission from(String input) {
-        return Arrays.stream(values())
-                .filter(mission -> mission.getName().equals(input))
-                .findFirst()
-                .orElseThrow(() -> PairmatchingException.from(ErrorMessage.INVALID_MISSION));
+    public Level getLevel() {
+        return level;
     }
 
     public String getName() {
         return name;
     }
-
-
 }
